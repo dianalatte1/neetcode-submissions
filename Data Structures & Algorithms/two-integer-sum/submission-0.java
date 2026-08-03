2de1f8ec70d0e6efd6d64da2202e1aa6f6140709
Diana Bautista Bautista <ing.diane.bb@gmail.com>
2026-08-02 21:40:48 -0600

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap <Integer, Integer> numbers = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int actualNumber = nums[i];
+            int complement = target - actualNumber;
+            if(numbers.containsKey(complement)){
+                return new int[] {numbers.get(complement), i};
+            }else{
+                numbers.put(actualNumber, i);
+
+            }
+
+        }
+
+        return new int[]{};
+
+        
+    }
+}
